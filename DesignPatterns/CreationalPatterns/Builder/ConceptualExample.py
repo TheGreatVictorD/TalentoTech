102a3ae414c75ca30744216bd90775d5d086c011
TheGreatVictorD <rsvr.120@gmail.com>
2024-04-22 12:45:14 -0500

    Initial commit

--- DesignPatterns/CreationalPatterns/Builder/ConceptualExample.py
+++ DesignPatterns/CreationalPatterns/Builder/ConceptualExample.py
@@ -3,12 +3,8 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 
+# Interfaz Builder
 class Builder(ABC):
-    """
-    The Builder interface specifies methods for creating the different parts of
-    the Product objects.
-    """
-
     @property
     @abstractmethod
     def product(self) -> None:
@@ -27,18 +23,9 @@ class Builder(ABC):
         pass
 
 
+# Constructores Concretos
 class ConcreteBuilder1(Builder):
-    """
-    The Concrete Builder classes follow the Builder interface and provide
-    specific implementations of the building steps. Your program may have
-    several variations of Builders, implemented differently.
-    """
-
     def __init__(self) -> None:
-        """
-        A fresh builder instance should contain a blank product object, which is
-        used in further assembly.
-        """
         self.reset()
 
     def reset(self) -> None:
@@ -46,20 +33,6 @@ class ConcreteBuilder1(Builder):
 
     @property
     def product(self) -> Product1:
-        """
-        Concrete Builders are supposed to provide their own methods for
-        retrieving results. That's because various types of builders may create
-        entirely different products that don't follow the same interface.
-        Therefore, such methods cannot be declared in the base Builder interface
-        (at least in a statically typed programming language).
-
-        Usually, after returning the end result to the client, a builder
-        instance is expected to be ready to start producing another product.
-        That's why it's a usual practice to call the reset method at the end of
-        the `getProduct` method body. However, this behavior is not mandatory,
-        and you can make your builders wait for an explicit reset call from the
-        client code before disposing of the previous result.
-        """
         product = self._product
         self.reset()
         return product
@@ -74,16 +47,8 @@ class ConcreteBuilder1(Builder):
         self._product.add("PartC1")
 
 
+# Producto
 class Product1:
-    """
-    It makes sense to use the Builder pattern only when your products are quite
-    complex and require extensive configuration.
-
-    Unlike in other creational patterns, different concrete builders can produce
-    unrelated products. In other words, results of various builders may not
-    always follow the same interface.
-    """
-
     def __init__(self) -> None:
         self.parts = []
 
@@ -94,14 +59,8 @@ class Product1:
         print(f"Product parts: {', '.join(self.parts)}", end="")
 
 
+# Director
 class Director:
-    """
-    The Director is only responsible for executing the building steps in a
-    particular sequence. It is helpful when producing products according to a
-    specific order or configuration. Strictly speaking, the Director class is
-    optional, since the client can control builders directly.
-    """
-
     def __init__(self) -> None:
         self._builder = None
 
@@ -111,17 +70,7 @@ class Director:
 
     @builder.setter
     def builder(self, builder: Builder) -> None:
-        """
-        The Director works with any builder instance that the client code passes
-        to it. This way, the client code may alter the final type of the newly
-        assembled product.
-        """
         self._builder = builder
-
-    """
-    The Director can construct several product variations using the same
-    building steps.
-    """
 
     def build_minimal_viable_product(self) -> None:
         self.builder.produce_part_a()
@@ -133,12 +82,6 @@ class Director:
 
 
 if __name__ == "__main__":
-    """
-    The client code creates a builder object, passes it to the director and then
-    initiates the construction process. The end result is retrieved from the
-    builder object.
-    """
-
     director = Director()
     builder = ConcreteBuilder1()
     director.builder = builder
@@ -155,7 +98,6 @@ if __name__ == "__main__":
 
     print("\n")
 
-    # Remember, the Builder pattern can be used without a Director class.
     print("Custom product: ")
     builder.produce_part_a()
     builder.produce_part_b()
